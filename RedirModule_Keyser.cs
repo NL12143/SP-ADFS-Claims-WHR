b3786c151266b11ca792ba2ad6f220df1b24b518
SharePointSan <NL12143@hotmail.com>
2018-01-02 14:46:12 +0100

    Add files via upload

---
+++ RedirModule_Keyser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Diagnostics;
+
+namespace AdfsHrd
+{
+    public class RedirModule : iHttpModule
+    {
+	    public void Dispose()
+	    {
+		    //throw new NotImplementedException();
+	    }
+	    public void Init(HttpApplication context)
+	    {
+		    try
+		    {
+                context.EndRequest += new EventHandler(EndRequest);
+		    }
+		    catch (Exception ex)
+		    {
+			    Debug.WriteLine(ex.Message);
+		    }
+	    }
+
+        public void EndRequest(object sender, EventArgs e)
+        {
+            try
+            {
+                //get the app and context sources
+                HttpApplication application = (HttpApplication)sender;
+                HttpContext context = application.Context;
+
+                if (context.Response.IsRequestBeingRedirected == true &&
+                    context.Request.QueryString.AllKeys.Contains("whr"))
+                {
+                    string whr = "whr=" + context.Request.QueryString["whr"];
+                    string redir = context.Response.RedirectLocation;
+                    if (!redir.Contains(whr))
+                    {
+                        redir = redir + (redir.Contains("?") ? "&" + whr : "?" + whr);
+                        context.Response.RedirectLocation = redir;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ RedirModule_Peshka.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Diagnostics;
+
+
+namespace AdfsHrd
+{
+	public class RedirModule : IHttpModule
+	{
+
+		//default endpoint
+		private const string DEFAULT_REALM = "http://geneva.vbtoys.com/adfs/services/trust";
+		
+
+		#region required interfaces
+		public void Dispose()
+		{
+			//throw new NotImplementedException();
+		}
+		#endregion
+
+
+		public void Init(HttpApplication context)
+		{
+			try
+			{
+				context.BeginRequest += new EventHandler(context_BeginRequest);
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+			}
+		}
+
+		void context_BeginRequest(object sender, EventArgs e)
+		{
+			try
+			{
+				//get the app and context sources
+				HttpApplication application = (HttpApplication)sender;
+				HttpContext context = application.Context;
+
+				Uri rqst = context.Request.Url;
+
+				//look for the /_trust/default.aspx page that does not have whr param yet
+				if (
+					(rqst.ToString().ToLower().Contains("_trust/default.aspx")) &&
+					(!context.Request.QueryString.AllKeys.Contains("whr"))
+					)
+				{
+					StringBuilder sb = new StringBuilder(1024);
+
+					//enumerate all query string values
+					foreach (string key in context.Request.QueryString.Keys)
+					{
+						sb.Append(key + "=" + context.Request.QueryString[key] + "&");
+					}
+
+					//THIS IS MY SIMPLE REDIRECTION FOR HLD; INSERT YOUR BUSINESS LOGIC
+					//HERE TO SEND THE USER TO THE LOCATION THEY SHOULD GO
+					//append the whr parameter
+					sb.Append("whr=" + DEFAULT_REALM);
+
+					//redirect with the new set of query string values
+					string redir = rqst.Scheme + "://" + rqst.Host + ":" +
+						rqst.Port.ToString() + rqst.AbsolutePath;
+					context.Response.Redirect(redir + "?" + sb.ToString());
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+			}
+		}
+
+
+	}
+}
